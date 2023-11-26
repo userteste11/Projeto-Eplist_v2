0fe099d7fe826c0f165b8a7ccc300bdececcb7fb
Lucas Gabriel <ltesteteste138@gmail.com>
2023-11-25 23:42:58 -0300

    front-maisFuçao

---
+++ FRONT/src/app/pages/Unidade/unidade-atualizar/unidade-atualizar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unidade-atualizar',
+  templateUrl: './unidade-atualizar.component.html',
+  styleUrls: ['./unidade-atualizar.component.css']
+})
+export class UnidadeAtualizarComponent {
+
+}
